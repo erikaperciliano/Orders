4fc4ac3127b8f2d1ab321b2a3f43279622d5c9a7
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-02-07 20:40:01 +0000

    add validation to className

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,4 @@
+import { CategoryButton } from '@/components/category-button'
 import Header from '@/components/header'
 import { View } from 'react-native'
 
@@ -5,6 +6,10 @@ export default function Home() {
   return (
     <View className="flex-1 pt-8">
       <Header title='Make your wish' cartQuantityItems={2}/>
+
+      <View className='flex-row gap-4'>
+        <CategoryButton title='Snack of the day' />
+      </View>
     </View>
   )
 }
