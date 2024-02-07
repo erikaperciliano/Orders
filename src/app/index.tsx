10c49d39062c56f985a63340dd76f0257f41455d
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-02-07 20:25:42 +0000

    add cart icon and catQuantityItems props

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { View } from 'react-native'
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Header title='Make your wish'/>
+      <Header title='Make your wish' cartQuantityItems={2}/>
     </View>
   )
 }
