453ca38679fe4b8bc5888b2a1fd2b4eac698d45c
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-02-07 20:14:15 +0000

    creates props to Header Component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,10 @@
 import Header from '@/components/header'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Header />
+      <Header title='Make your wish'/>
     </View>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,20 @@
 import { Image, View, Text } from 'react-native'
+import tw from 'tailwind-rn';
 
-export default function Header() {
+type HeaderProps = {
+    title: string
+}
+
+export default function Header({ title }: HeaderProps) {
   return (
     <View className='flex-row items-center border-b border-slate-700 pb-5 mx-5'>
         <View className='flex-1'>
             <Image source={require('@/assets/logo.png')} className='h-6 w-32' />
-            <Text className='text-white text-xl font-heading mt-2'>Menu</Text>
+            <Text className='text-white text-xl font-heading mt-2'>{title}</Text>
+        </View>
+
+        <View className='bg-lime-300 w-4 h-4 rounded-full'>
+            <Text>1</Text>
         </View>
     </View>
   )
