7fbd28a18c945083f1dd05eba11bf9b880a27aef
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-02-08 17:46:48 +0000

    shows the information about the product

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,6 +1,10 @@
 import { Image, View, Text } from 'react-native'
 import { useLocalSearchParams } from 'expo-router'
 import { PRODUCTS } from '@/utils/data/products'
+import { formatCurrency } from '@/utils/functions/format-cuttency'
+import { Button } from '@/components/button'
+import { Feather } from '@expo/vector-icons'
+import { LinkButton } from '@/components/link-button'
 
 export default function Product(){
     const { id } = useLocalSearchParams()
@@ -16,7 +20,29 @@ export default function Product(){
             />
 
             <View className='p-5 mt-8 flex-1'>
-                <Text className='text-lime-400 text-2xl font-heading my-2'>{product.price}</Text>
+                <Text className='text-lime-400 text-2xl font-heading my-2'>{formatCurrency(product.price)}</Text>
+
+                <Text className='text-slate-400 font-body text-base leading-6 mb-6'>{product.description}</Text>
+            
+                {
+                    product.ingredients.map((ingredient) => (
+                        <Text key={ingredient} className='text-slate-400 font-body text-base leading-6'>{'\u2022'} {ingredient}</Text>
+                    ))
+                }
+            </View>
+
+            <View className='p-5 pb-8 gap-5'>
+                <Button>
+                    <Button.Icon>
+                        <Feather name='plus-circle' size={20}/>
+                    </Button.Icon>
+
+                    <Button.Text>
+                        Add to order
+                    </Button.Text>   
+                </Button>
+
+                <LinkButton  title='Return to menu' href='/' />
             </View>
         </View>
     )
