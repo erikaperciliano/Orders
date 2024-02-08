0ac3d05031993f12b6d116fcfc921663e73b1292
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-02-08 20:17:54 +0000

    persisting the data

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,5 +1,10 @@
 import { ProductProps } from "@/utils/data/products";
+
 import { create } from "zustand";
+import { createJSONStorage, persist } from 'zustand/middleware'
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 import * as cartInMemory from './helpers/cart-in-memory'
 
 export type ProductCartProps = ProductProps & {
@@ -13,12 +18,18 @@ type StateProps = {
 }
 
 // will be used to save and share content throughout the application
-export const useCartStore = create<StateProps>((set) => ({
-    products: [],
-    add: (product: ProductProps) => set((state) => ({
-        products: cartInMemory.add(state.products, product)
-    })),
-    remove: (productId: string) => set((state) => ({
-        products: cartInMemory.remove(state.products, productId)
-    }))
-}))
+export const useCartStore = create(
+    persist<StateProps>((set) => ({
+        products: [],
+        add: (product: ProductProps) => set((state) => ({
+            products: cartInMemory.add(state.products, product)
+        })),
+        remove: (productId: string) => set((state) => ({
+            products: cartInMemory.remove(state.products, productId)
+        }))
+    }),
+    {
+        name: 'orders:cart',
+        storage: createJSONStorage(() => AsyncStorage)
+    }
+))
