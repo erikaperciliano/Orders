715f019fd8937ad4a2aa5e087ad08f1976e333cb
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-02-08 17:07:37 +0000

    creates id to product

---
+++ src/app/product/[id].tsx
@@ -0,0 +1,23 @@
+import { Image, View, Text } from 'react-native'
+import { useLocalSearchParams } from 'expo-router'
+import { PRODUCTS } from '@/utils/data/products'
+
+export default function Product(){
+    const { id } = useLocalSearchParams()
+
+    const product = PRODUCTS.filter((item) => item.id === id)[0]
+
+    return(
+        <View className={'flex-1'}>
+            <Image 
+                source={product.cover} 
+                className='w-full h-52'
+                resizeMode='cover'
+            />
+
+            <View className='p-5 mt-8 flex-1'>
+                <Text className='text-lime-400 text-2xl font-heading my-2'>{product.price}</Text>
+            </View>
+        </View>
+    )
+}
--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,4 +1,5 @@
 import { Image, ImageProps, TouchableOpacity, TouchableOpacityProps, View, Text } from 'react-native'
+import { forwardRef } from 'react'
 
 type ProductDataprops = {
    title: string
@@ -11,15 +12,16 @@ type ProductProps = TouchableOpacityProps & {
     data: ProductDataprops
 }
 
-export function Product({ data, ...rest }: ProductProps) {
+export const Product = forwardRef<TouchableOpacity, ProductProps>(({ data, ...rest },
+    ref) => {
   return (
-    <TouchableOpacity className='w-full flex-row items-center pb-4' {...rest}>
+    <TouchableOpacity ref={ref} className='w-full flex-row items-center pb-4' {...rest}>
         <Image source={data.thumbnail} className='w-20 h-20 rounded-md'/>
 
         <View className='flex-1 ml-3'>
             <Text className='text-slate-100 font-subtitle text-base flex-1'>{data.title}</Text>
             <Text className='text-slate-400 text-xs leading-5 mt-0.5'>{data.description}</Text>
         </View>
-    </TouchableOpacity>
+    </TouchableOpacity> 
   )
-}
+})
